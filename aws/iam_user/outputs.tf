17aef9715b0a3ae5052e511e935d552288ef0909
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-11 10:55:56 -0600

    cleanup

--- aws/iam_user/outputs.tf
+++ aws/iam_user/outputs.tf
@@ -2,7 +2,7 @@
 output "users_info" {
   description = "A map of usernames to their respective ARNs and passwords."
   value = {
-    for user, details in aws_iam_user.user : user => {
+    for user, details in var.users : user => {
       arn      = details.arn
       username = details.name
       password = aws_iam_user_login_profile.this[user].encrypted_password
