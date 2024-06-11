49925fdbc1740f54e726e334554851f2b64622c3
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-10 20:37:27 -0600

    cleanup

--- aws/iam_user/outputs.tf
+++ aws/iam_user/outputs.tf
@@ -1,7 +1,7 @@
 output "users_info" {
   description = "A map of usernames to their respective ARNs and passwords."
   value = { for user in var.users :
-    user.name => {
+    user.key => {
       arn      = user.arn,
     }
   }
