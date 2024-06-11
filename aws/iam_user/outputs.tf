d367d666f29c4e708a91406571da1756b8dc56f5
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-11 13:21:58 -0600

    cleanup

---
+++ aws/iam_user/outputs.tf
@@ -0,0 +1,19 @@
+# Output user information
+output "password" {
+  description = "The username and encrypted password of the created IAM users"
+  value = {
+    for username, login_profile in aws_iam_user_login_profile.this :
+    username => {
+      password = login_profile.encrypted_password
+      username = username
+    } 
+  }
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.this.encrypted_password
+}
+
+output "login_profile_count" {
+  value = length(aws_iam_user_login_profile.this)
+}
