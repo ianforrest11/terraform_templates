237449dda946c2b8ec0ab390d3cd5d67089b6856
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-13 09:52:36 -0600

    adding identity provider

--- aws/iam_identity_provider/variables.tf
+++ aws/iam_identity_provider/variables.tf
@@ -1,9 +1,8 @@
 variable "identity_provider" {
-  description = "A map of identity provider"
-  type = map(object({
+  description = "Identity provider configuration"
+  type = object({
     client_id_list  = list(string)
     provider_url    = string
     thumbprint_list = list(string)
-  }))
-  default     = {}
+  })
 }
