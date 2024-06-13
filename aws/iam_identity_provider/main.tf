05699a039276b738b26a7dc1e571a767df33e85b
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-13 10:07:28 -0600

    adding identity provider

--- aws/iam_identity_provider/main.tf
+++ aws/iam_identity_provider/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "this" {
   url = var.identity_provider.provider_url
   client_id_list = var.identity_provider.client_id_list
-  thumbprint_list = var.thumbprint_list.thumbprint_list
+  thumbprint_list = var.identity_provider.thumbprint_list
 }
