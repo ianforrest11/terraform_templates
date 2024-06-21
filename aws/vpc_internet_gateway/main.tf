bd6985faf8fa8861d4edfa1fe10d66ef1c2b38b4
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-21 14:29:47 -0600

    internet gateway

---
+++ aws/vpc_internet_gateway/main.tf
@@ -0,0 +1,6 @@
+resource "aws_internet_gateway" "this" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = var.name
+  }
+}
---
+++ aws/vpc_internet_gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  nullable = false
+  type     = string
+}
+
+variable "vpc_id" {
+  nullable = false
+  type     = string
+}
