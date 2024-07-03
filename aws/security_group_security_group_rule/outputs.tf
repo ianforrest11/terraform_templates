4af202db72012ffa18f5a130599c3dbf99fb8929
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-03 11:18:06 -0600

    updating security groups and adding security group rules

---
+++ aws/security_group_security_group_rule/outputs.tf
@@ -0,0 +1,4 @@
+output "id" {
+  description = "The ID of the security group rule"
+  value       = aws_security_group_rule.this.id
+}
---
+++ aws/security_group_security_group_rule/variables.tf
@@ -0,0 +1,60 @@
+variable "type" {
+  description = "The type of rule, ingress or egress"
+  type        = string
+}
+
+variable "from_port" {
+  description = "The start port (or ICMP type number if protocol is icmp or icmpv6)"
+  type        = number
+}
+
+variable "to_port" {
+  description = "The end port (or ICMP code if protocol is icmp or icmpv6)"
+  type        = number
+}
+
+variable "protocol" {
+  description = "The protocol. If not icmp, icmpv6, tcp, udp, or all use the corresponding protocol number"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "The security group to apply this rule to"
+  type        = string
+}
+
+variable "cidr_blocks" {
+  description = "List of CIDR blocks"
+  type        = list(string)
+  default     = []
+}
+
+variable "ipv6_cidr_blocks" {
+  description = "List of IPv6 CIDR blocks"
+  type        = list(string)
+  default     = []
+}
+
+variable "prefix_list_ids" {
+  description = "List of prefix list IDs (for allowing access to VPC endpoints)"
+  type        = list(string)
+  default     = []
+}
+
+variable "source_security_group_id" {
+  description = "Security group id to allow traffic from"
+  type        = string
+  default     = null
+}
+
+variable "self" {
+  description = "If true, the security group itself will be added as a source to this ingress rule"
+  type        = bool
+  default     = false
+}
+
+variable "description" {
+  description = "Description of this rule"
+  type        = string
+  default     = null
+}
