90cb658ea5c8887983b6526d25704765df1906d6
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-16 14:05:48 -0600

    pushing secret templates

--- aws/ec2_key_pair/main.tf
+++ aws/ec2_key_pair/main.tf
@@ -1,21 +1,5 @@
-# Generate an SSH key pair
-resource "tls_private_key" "this" {
-  algorithm = var.algorithm
-  rsa_bits  = var.rsa_bits
-}
-
 # save to aws resource
 resource "aws_key_pair" "this" {
   key_name   = var.key_name
-  public_key = tls_private_key.this.public_key_openssh
-}
-
-# Store the private key in AWS Secrets Manager
-resource "aws_secretsmanager_secret" "this" {
-  name = "${var.secret_name_prefix}_private"
-}
-
-resource "aws_secretsmanager_secret_version" "this" {
-  secret_id     = aws_secretsmanager_secret.this.id
-  secret_string = tls_private_key.this.private_key_pem
+  public_key = var.public_key
 }
--- aws/ec2_key_pair/variables.tf
+++ aws/ec2_key_pair/variables.tf
@@ -9,23 +9,3 @@ variable "public_key" {
   type        = string
   nullable    = false
 }
-
-variable "algorithm" {
-  description = "The algorithm to use for the SSH key pair"
-  type        = string
-  default     = "RSA"
-  nullable    = false
-}
-
-variable "rsa_bits" {
-  description = "The number of bits for the RSA key"
-  type        = number
-  default     = 4096
-  nullable    = false
-}
-
-variable "secret_name_prefix" {
-  description = "name of secret"
-  type        = string
-  nullable    = false
-}
