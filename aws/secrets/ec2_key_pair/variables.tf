6abb280712d476c97729428aebaa79b345fb7d59
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-16 12:32:25 -0600

    pushing secret templates

--- aws/secrets/ec2_key_pair/variables.tf
+++ aws/secrets/ec2_key_pair/variables.tf
@@ -1,15 +1,3 @@
-variable "key_name" {
-  description = "The name to assign to the SSH key pair"
-  type        = string
-  nullable    = false
-}
-
-variable "public_key" {
-  description = "The public key material for the SSH key pair"
-  type        = string
-  nullable    = false
-}
-
 variable "algorithm" {
   description = "The algorithm to use for the SSH key pair"
   type        = string
