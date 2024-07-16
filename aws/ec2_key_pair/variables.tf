d0f285c30c6f08319ec3653c26033c43063c4e3c
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-16 10:43:14 -0600

    update secret

--- aws/ec2_key_pair/variables.tf
+++ aws/ec2_key_pair/variables.tf
@@ -9,3 +9,24 @@ variable "public_key" {
   type        = string
   nullable    = false
 }
+
+variable "algorithm" {
+  description = "The algorithm to use for the SSH key pair"
+  type        = string
+  default     = "RSA"
+  nullable    = false
+}
+
+variable "rsa_bits" {
+  description = "The number of bits for the RSA key"
+  type        = number
+  default     = 4096
+  nullable    = false
+}
+
+variable "secret_name_prefix" {
+  description = "The algorithm to use for the SSH key pair"
+  type        = string
+  default     = "RSA"
+  nullable    = false
+}
