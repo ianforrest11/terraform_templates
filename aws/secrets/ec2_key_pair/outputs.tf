e4a1f9e6d2a2ab3ca69b61c2f2342d69226af16b
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-16 13:41:45 -0600

    pushing secret templates

--- aws/secrets/ec2_key_pair/outputs.tf
+++ aws/secrets/ec2_key_pair/outputs.tf
@@ -1,9 +1,1 @@
-output "key_name" {
-  description = "The name of the SSH key pair"
-  value       = aws_key_pair.this.key_name
-}
 
-output "key_fingerprint" {
-  description = "The fingerprint of the SSH key pair"
-  value       = aws_key_pair.this.key_fingerprint
-}
