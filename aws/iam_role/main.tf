8f09c02a20c21377a1dab4f1e2a963ead8b84e1d
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-18 10:37:52 -0600

    updating mapping of policies to roles

--- aws/iam_role/main.tf
+++ aws/iam_role/main.tf
@@ -1,9 +1,7 @@
 resource "aws_iam_role" "this" {
   for_each = var.roles
   name     = each.key
-
   assume_role_policy = file("../json/${each.value.assume_role_policy_name}.json")
-
   tags = {
     environment = each.value.environment
     purpose     = each.value.purpose
@@ -33,7 +31,7 @@ locals {
 # Attach policies to roles
 resource "aws_iam_role_policy_attachment" "this" {
   for_each = {
-    for idx, role_policy in local.flattened_role_policy_map : "${role_policy.role}-${idx}" => role_policy
+    for role_policy in local.flattened_role_policy_map : "${role_policy.role}-${role_policy.policy_arn}" => role_policy
   }
   role       = each.value.role
   policy_arn = each.value.policy_arn
