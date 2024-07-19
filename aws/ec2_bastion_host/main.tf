bf08b48519d0b50a412f8343aae678f33cdcb518
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-19 10:26:41 -0600

    pushing bastion host

--- aws/ec2_bastion_host/main.tf
+++ aws/ec2_bastion_host/main.tf
@@ -8,27 +8,5 @@ resource "aws_instance" "bastion" {
     Name         = var.instance_name
     Environment  = var.environment
   }
-  security_groups = [aws_security_group.bastion_sg.name]
-}
-
-resource "aws_security_group" "bastion_sg" {
-  name        = "${var.environment}_bastion_sg"
-  description = "Security group ${var.environment} for bastion host"
-  vpc_id      = var.vpc_id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ssh_cidr_blocks
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.environment}_bastion_sg"
-  }
+  security_groups = var.security_group_ids
 }
--- aws/ec2_bastion_host/variables.tf
+++ aws/ec2_bastion_host/variables.tf
@@ -47,3 +47,9 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
+variable "security_group_ids" {
+  description = "A list of security group IDs to associate with the EKS cluster."
+  nullable    = false
+  type        = list(string)
+}
