f8d8a4a458e37839e5f6bc3b2d0f8b6e8f11e273
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-19 08:33:36 -0600

    pushing bastion host

---
+++ aws/ec2_bastion_host/variables.tf
@@ -0,0 +1,55 @@
+variable "region" {
+  description = "AWS region to deploy the bastion host"
+  type        = string
+  default     = "us_west_2"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the bastion host"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the bastion host"
+  type        = string
+  default     = "t3_micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the bastion host"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the subnet is located"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key name for SSH access"
+  type        = string
+}
+
+variable "allowed_ssh_cidr_blocks" {
+  description = "List of CIDR blocks allowed to SSH into the bastion host"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "associate_public_ip_address" {
+  description = "Associate a public IP address with the bastion host"
+  type        = bool
+  default     = true
+}
+
+variable "instance_name" {
+  description = "Name tag for the bastion host instance"
+  type        = string
+  default     = "bastion_host"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
