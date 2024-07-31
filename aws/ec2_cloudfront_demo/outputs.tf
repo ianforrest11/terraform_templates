22cece561cef57b9eb031c7a519f88cc27967918
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-31 08:59:59 -0600

    adding ec2 cloudfront demo template

--- aws/ec2_cloudfront_demo/outputs.tf
+++ aws/ec2_cloudfront_demo/outputs.tf
@@ -1,6 +1,6 @@
-output "ec2_cloudfront_demo_public_ip" {
+output "ec2_cloudfront_demo_public_dns" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.ec2_cloudfront_demo.public_ip
+  value       = aws_instance.ec2_cloudfront_demo.public_dns
 }
 
 output "ec2_cloudfront_demo_instance_id" {
