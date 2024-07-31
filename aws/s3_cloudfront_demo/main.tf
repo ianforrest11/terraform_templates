4e30a84b673d42be8ae03de18614026b2a1ecfe2
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-31 17:02:07 -0600

    fixing s3

--- aws/s3_cloudfront_demo/main.tf
+++ aws/s3_cloudfront_demo/main.tf
@@ -1,40 +1,36 @@
-resource "aws_s3_bucket" "ec2_cloudfront_demo" {
+resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
-  force_destroy  = var.force_destroy
-  tags = var.bucket_tags
+  acl    = "private"
 }
 
-resource "aws_s3_bucket_logging" "ec2_cloudfront_demo" {
-  bucket        = aws_s3_bucket.ec2_cloudfront_demo.id
-  target_bucket = aws_s3_bucket.ec2_cloudfront_demo.id
-  target_prefix = var.target_prefix
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
-resource "aws_s3_bucket_policy" "ec2_cloudfront_demo" {
-  bucket = aws_s3_bucket.ec2_cloudfront_demo.id
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:*"]
+    resources = [
+      "${aws_s3_bucket.example.arn}/*",
+      aws_s3_bucket.example.arn,
+    ]
 
-  policy = templatefile("${path.module}/s3_policy.json", {
-    bucket_name      = var.bucket_name
-    aws_account_id   = var.aws_account_id
-  })
-}
-
-resource "aws_s3_bucket_acl" "ec2_cloudfront_demo" {
-  bucket = aws_s3_bucket.ec2_cloudfront_demo.id
-
-  access_control_policy {
-    owner {
-      display_name = var.owner_display_name
-      id           = var.owner_id
+    principals {
+      type        = "AWS"
+      identifiers = [var.user_arn]
     }
+  }
 
-    grant {
-      grantee {
-        id   = var.grantee_id
-        type = "CanonicalUser"
-        uri  = var.grantee_uri
-      }
-      permission = "FULL_CONTROL"
+  statement {
+    actions = ["s3:GetObject"]
+    resources = [
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
   }
 }
--- aws/s3_cloudfront_demo/variables.tf
+++ aws/s3_cloudfront_demo/variables.tf
@@ -1,41 +1,9 @@
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-  default     = "ian-cloudfront-demo-logs-bucket-ap"
-}
-
-variable "force_destroy" {
-  description = "Force destroy the bucket"
-  type        = bool
-  default     = true
-}
-
-variable "target_prefix" {
-  description = "Target prefix for S3 bucket logging"
-  type        = string
-  default     = "log/"
-}
-
-variable "bucket_tags" {
-  description = "Tags for the S3 bucket"
-  type        = map(string)
-  default = {
-    Name        = "CloudFrontLogs"
-    Environment = "Production"
-  }
-}
-
-variable "aws_account_id" {
-  description = "AWS Account ID"
+  description = "The name of the S3 bucket"
   type        = string
 }
 
-variable "owner_display_name" {
-  description = "Display name of the bucket owner"
-  type        = string
-}
-
-variable "owner_id" {
-  description = "ID of the bucket owner"
+variable "user_arn" {
+  description = "The ARN of the IAM user to allow access"
   type        = string
 }
