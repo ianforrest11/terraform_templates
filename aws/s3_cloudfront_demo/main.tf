eb4c1898140470cb9a2669d1752a78ab62a1eb5f
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-08-01 09:30:41 -0600

    fixing s3

--- aws/s3_cloudfront_demo/main.tf
+++ aws/s3_cloudfront_demo/main.tf
@@ -1,20 +1,15 @@
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
-
   tags = {
     Name = var.bucket_name
   }
+  acl = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
   policy = templatefile("${path.module}/policy.json", {
-    bucket_arn = aws_s3_bucket.example.arn
-    user_arn   = var.user_arn
+    bucket_arn     = aws_s3_bucket.example.arn,
+    user_arn       = var.user_arn
   })
-}
-
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "log-delivery-write"
 }
