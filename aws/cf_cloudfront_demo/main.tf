0cb97c9ec2ae595119322c0932f8aea105a817a3
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-08-01 12:37:30 -0600

    fixing s3

---
+++ aws/cf_cloudfront_demo/main.tf
@@ -0,0 +1,59 @@
+resource "aws_cloudfront_distribution" "example" {
+  origin {
+    domain_name = var.origin_domain
+    origin_id   = var.origin_name
+
+    custom_origin_config {
+      http_port              = var.http_port
+      https_port             = var.https_port
+      origin_protocol_policy = var.origin_protocol_policy
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  price_class         = var.price_class
+
+  default_cache_behavior {
+    target_origin_id       = var.origin_name
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = var.allowed_http_methods
+    cached_methods  = ["GET", "HEAD"]
+
+    cache_policy_id             = var.cache_policy_id
+    origin_request_policy_id    = var.origin_request_policy_id
+    compress                    = var.compress_objects
+    smooth_streaming            = false
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  logging_config {
+    include_cookies = false
+    bucket          = var.logging_bucket
+    prefix          = var.logging_prefix
+  }
+
+  http_version = var.supported_http_versions
+
+  tags = {
+    Name = var.origin_name
+  }
+}
---
+++ aws/cf_cloudfront_demo/variables.tf
@@ -0,0 +1,74 @@
+variable "origin_domain" {
+  description = "The origin domain (public DNS)"
+  type        = string
+}
+
+variable "origin_protocol_policy" {
+  description = "The protocol to use"
+  type        = string
+  default     = "match-viewer"
+}
+
+variable "http_port" {
+  description = "The port to use for the http protocol"
+  type        = number
+  default     = 80
+}
+
+variable "https_port" {
+  description = "The port to use for the https protocol"
+  type        = number
+  default     = 443
+}
+
+variable "origin_name" {
+  description = "The origin name"
+  type        = string
+}
+
+variable "compress_objects" {
+  description = "Whether to automatically compress objects"
+  type        = bool
+  default     = false
+}
+
+variable "allowed_http_methods" {
+  description = "Allowed HTTP methods"
+  type        = list(string)
+  default     = ["GET", "HEAD"]
+}
+
+variable "cache_policy_id" {
+  description = "Cache policy ID to use origin cache control headers"
+  type        = string
+  default     = "658327ea-f89d-4fab-a63d-7e88639e58f6" # AWS managed cache policy ID for 'Use Origin Cache Headers'
+}
+
+variable "origin_request_policy_id" {
+  description = "Origin request policy ID for all viewer"
+  type        = string
+  default     = "b689b0a8-53d0-40ab-baf2-68738e2966ac" # AWS managed origin request policy ID for 'All Viewer'
+}
+
+variable "price_class" {
+  description = "The price class for the distribution"
+  type        = string
+  default     = "PriceClass_100" # Use only North America and Europe
+}
+
+variable "supported_http_versions" {
+  description = "Supported HTTP versions"
+  type        = string
+  default     = "http2and3"
+}
+
+variable "logging_bucket" {
+  description = "The S3 bucket for standard logging"
+  type        = string
+}
+
+variable "logging_prefix" {
+  description = "The logging prefix for standard logging"
+  type        = string
+  default     = "/logs/"
+}
