33444f2d6fca6238a04c9a9f58f93341857bc2cb
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-08-01 09:45:58 -0600

    fixing s3

--- aws/s3_cloudfront_demo/main.tf
+++ aws/s3_cloudfront_demo/main.tf
@@ -1,24 +1,45 @@
+data "aws_canonical_user_id" "current" {}
+
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
   tags = {
     Name = var.bucket_name
   }
-  acl = "log-delivery-write"
-  depends_on = [aws_s3_bucket_ownership_controls.example]
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.example.bucket
 
-  rule {
-    object_ownership = "ObjectWriter"
+  access_control_policy {
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+
+    grant {
+      grantee {
+        id   = data.aws_canonical_user_id.current.id
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
+
+    grant {
+      # Grant CloudFront logs access to your Amazon S3 Bucket
+      # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
+      grantee {
+        id   = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+        type = "CanonicalUser"
+      }
+      permission = "FULL_CONTROL"
+    }
   }
 }
 
-resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id
-  policy = templatefile("${path.module}/policy.json", {
-    bucket_arn     = aws_s3_bucket.example.arn,
-    user_arn       = var.user_arn
-  })
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.example.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
